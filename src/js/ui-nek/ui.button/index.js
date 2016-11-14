5675c0b36300e45186e7d6e7425976901740db9c
Cody Chan <int64ago@gmail.com>
2016-11-14 21:58:01 +0800

    Save 4836NEK in data

--- src/js/ui-nek/ui.button/index.js
+++ src/js/ui-nek/ui.button/index.js
@@ -23,37 +23,35 @@ var UIButton = Component.extend({
         this.supr();
     },
     $$NEK: function() {
-        var data = this.data;
-        return {
+        this.data.$$NEK = {
             id: 100,
-            cols: 1,
-            conf: [
-                {
-                    key: 'title',
-                    // 默认值，默认取组件默认值
-                    default: data.title || '',
-                    // 配置描述
-                    desc: '标题',
-                    // none: radio
-                    // string: input-text
-                    // number: input-number
-                    // boolean: radio
-                    // select: select
-                    // array: checkbox
-                    // expresion: input-text -> key={exp}
-                    type: 'string',
-                    // 只有在 type 是 select/array 的时候才有意义
-                    selects: [],
-                },
-                {
-                    key: 'type',
-                    default: data.type || 'default',
-                    desc: '类型',
-                    type: 'select',
-                    selects: ['default', 'primary', 'info', 'success', 'warning', 'error'],
-                },
-            ]
-        }
+            name: this.name,
+            desc: '按钮',
+            layout: { cols: 1 },
+            conf: [{
+                key: 'title',
+                value: this.data.title,
+                // 配置描述
+                desc: '标题',
+                // none: radio
+                // string: input-text
+                // number: input-number
+                // boolean: radio
+                // select: select
+                // array: checkbox
+                // expresion: input-text -> key={exp}
+                type: 'string',
+                // 只有在 type 是 select/array 的时候才有意义
+                selects: [],
+            }, {
+                key: 'type',
+                value: this.data.type,
+                desc: '类型',
+                type: 'select',
+                selects: ['default', 'primary', 'info', 'success', 'warning', 'error'],
+            }]
+        };
+        return this.data.$$NEK;
     }
 });
 
