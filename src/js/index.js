16cfa6670f8d1174b1ffcf6984295af9df798e3c
360783395@qq.com <360783395@qq.com>
2016-12-06 22:13:19 +0800

    misc:合并unit和module为components， 新增进销存菜单

--- src/js/index.js
+++ src/js/index.js
@@ -14,48 +14,45 @@ exports._ = require('./ui-base/_.js');
 exports.ajax = require("./ui-base/ajax.js");
 exports.Validation = require("./util/validation.js");
 
-exports.Dropdown = require("./unit/navigation/dropdown.js");
-exports.Menu = require("./unit/navigation/menu.js");
+exports.Dropdown = require("./components/navigation/dropdown.js");
+exports.Menu = require("./components/navigation/menu.js");
 
-exports.UIInput = require("./unit/form/ui.input.js");
-exports.UITextArea = require("./unit/form/ui.textarea.js");
-exports.NumberInput = require("./unit/form/numberInput.js");
-exports.Check = require("./unit/form/check.js");
-exports.CheckGroup = require("./unit/form/checkGroup.js");
-exports.RadioGroup = require("./unit/form/radioGroup.js");
-exports.UISelect = require("./unit/form/ui.select.js");
-exports.SelectGroup = require("./unit/form/selectGroup.js");
-exports.TreeSelect = require("./unit/form/treeSelect.js");
-exports.Suggest = require("./unit/form/suggest.js");
-exports.Uploader = require("./unit/form/uploader.js");
-exports.UIButton = require("./unit/form/ui.button.js");
-exports.UIText = require("./unit/form/ui.text.js");
+exports.UIInput = require("./components/form/ui.input.js");
+exports.UITextArea = require("./components/form/ui.textarea.js");
+exports.NumberInput = require("./components/form/numberInput.js");
+exports.Check = require("./components/form/check.js");
+exports.CheckGroup = require("./components/form/checkGroup.js");
+exports.RadioGroup = require("./components/form/radioGroup.js");
+exports.UISelect = require("./components/form/ui.select.js");
+exports.SelectGroup = require("./components/form/selectGroup.js");
+exports.TreeSelect = require("./components/form/treeSelect.js");
+exports.Suggest = require("./components/form/suggest.js");
+exports.Uploader = require("./components/form/uploader.js");
+exports.UIButton = require("./components/form/ui.button.js");
+exports.UIText = require("./components/form/ui.text.js");
 
-exports.DatePicker = require("./unit/calendar/datePicker.js");
-exports.TimePicker = require("./unit/calendar/timePicker.js");
-exports.DateTimePicker = require("./unit/calendar/dateTimePicker.js");
+exports.DatePicker = require("./components/calendar/datePicker.js");
+exports.TimePicker = require("./components/calendar/timePicker.js");
+exports.DateTimePicker = require("./components/calendar/dateTimePicker.js");
 
-exports.Progress = require("./unit/widget/progress.js");
-exports.Loading = require("./unit/widget/loading.js");
-exports.Gotop = require("./unit/widget/gotop.js");
-exports.Hint = require("./unit/widget/hint.js");
+exports.Progress = require("./components/widget/progress.js");
+exports.Loading = require("./components/widget/loading.js");
+exports.Gotop = require("./components/widget/gotop.js");
+exports.Hint = require("./components/widget/hint.js");
 
-exports.Tabs = require("./module/navigation/tabs.js");
-exports.Collapse = require("./module/navigation/collapse.js");
-exports.Pager = require("./module/navigation/pager.js");
+exports.Tabs = require("./components/navigation/tabs.js");
+exports.Collapse = require("./components/navigation/collapse.js");
+exports.Pager = require("./components/navigation/pager.js");
 
-exports.Notify = require("./module/frame/notify.js");
-exports.Modal = require("./module/frame/modal.js");
+exports.Notify = require("./components/frame/notify.js");
+exports.Modal = require("./components/frame/modal.js");
 
-exports.ListView = require("./module/data.view/listView.js");
-exports.UltiListView = require("./module/data.view/ultiListView.js");
-exports.TreeView = require("./module/data.view/treeView.js");
-exports.MultiTreeView = require("./module/data.view/multiTreeView.js");
+exports.ListView = require("./components/data.view/listView.js");
+exports.UltiListView = require("./components/data.view/ultiListView.js");
+exports.TreeView = require("./components/data.view/treeView.js");
+exports.MultiTreeView = require("./components/data.view/multiTreeView.js");
 
-exports.Calendar = require("./module/calendar/calendar.js");
+exports.Calendar = require("./components/calendar/calendar.js");
 
-exports.HTMLEditor = require("./module/editor/htmlEditor.js");
-exports.MarkEditor = require("./module/editor/markEditor.js");
-
-exports.UIForm = require("./module/form/ui.form.js");
-exports.UIField = require("./module/form/ui.field.js");
+exports.UIForm = require("./components/form/ui.form.js");
+exports.UIField = require("./components/form/ui.field.js");
