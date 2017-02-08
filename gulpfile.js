2cfcbab678c607e09cbd52d14b51a60feb7378c3
Cody Chan <int64ago@gmail.com>
2017-02-08 19:28:40 +0800

    style: format gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,67 +15,58 @@ var path = require('path');
 var Hexo = require('hexo');
 var fs = require('fs');
 var doc = require('./src/js/components/doc');
+var themes = require('./src/mcss/themes');
 
 var hexo = new Hexo(process.cwd(), {});
 hexo.init();
 
-var themes = [
-    'default',
-    'red',
-    'blue',
-];
-
-var browserSync =require('browser-sync').create();
-gulp.THEMES = ['default'];
-
-
-/**
- * ------------------------------------------------------------
- * Build Dist
- * ------------------------------------------------------------
- */
+var browserSync = require('browser-sync').create();
 
 gulp.task('dist-clean', function(cb) {
-    rimraf('{dist,public}', function() {
-      rimraf('doc/components/_*', cb)
-    })
+  rimraf('{dist,public}', function() {
+    rimraf('doc/components/_*', cb)
+  })
 });
 
 gulp.task('dist-copy', function() {
-    return all(
-        gulp.src('./node_modules/font-awesome/fonts/**').pipe(gulp.dest('./dist/fonts'))
-    );
+  return all(
+    gulp.src('./node_modules/font-awesome/fonts/**').pipe(gulp.dest('./dist/fonts'))
+  );
 });
 
 gulp.task('dist-js', function() {
-    return gulp.src('./src/js/index.js')
-        .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest('./dist/js'))
-        .pipe(ignore.exclude('*.js.map'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'));
+  return gulp.src('./src/js/index.js')
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(ignore.exclude('*.js.map'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('dist-css', function() {
-    var gulpCSS = function(theme) {
-        return gulp.src('./src/mcss/' + theme + '.mcss')
-            .pipe(mcss({
-                pathes: ['./node_modules'],
-                importCSS: true
-            }))
-            .pipe(rename('nek-ui.' + theme + '.css'))
-            .pipe(gulp.dest('./dist/css'))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(minifycss())
-            .pipe(gulp.dest('./dist/css'));
-    }
-    
-    return all(themes.map(gulpCSS));
+  var gulpCSS = function(theme) {
+    return gulp.src('./src/mcss/' + theme + '.mcss')
+      .pipe(mcss({
+        pathes: ['./node_modules'],
+        importCSS: true
+      }))
+      .pipe(rename('nek-ui.' + theme + '.css'))
+      .pipe(gulp.dest('./dist/css'))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(minifycss())
+      .pipe(gulp.dest('./dist/css'));
+  }
+
+  return all(themes.map(gulpCSS));
 });
 
 gulp.task('gen-mcss', function(cb) {
-  glob(path.join(__dirname, './src/js/components/**/**/*.mcss'), function (er, files) {
+  glob(path.join(__dirname, './src/js/components/**/**/*.mcss'), function(er, files) {
     var out = '';
     files.forEach(function(d) {
       out += '@import "' + d + '";\n';
@@ -92,20 +83,20 @@ gulp.task('gen-doc', function(cb) {
 });
 
 gulp.task('default', function(done) {
-    sequence('dist-clean', ['dist-copy', 'gen-mcss', 'dist-js', 'dist-css'], 'gen-doc', done);
+  sequence('dist-clean', ['dist-copy', 'gen-mcss', 'dist-js', 'dist-css'], 'gen-doc', done);
 });
 
-gulp.task('server', ['default'], function(){
-    browserSync.init({
-		server: {
-		baseDir: './public'
-		},
-		browser: 'default',
-		reloadDelay: 1000,
-		port:8089
-    });
+gulp.task('server', ['default'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './public'
+    },
+    browser: 'default',
+    reloadDelay: 1000,
+    port: 8089
+  });
 });
 
-gulp.task('watch', ['server'], function () {
-    gulp.watch(['./src/**/*'], ['default']);
+gulp.task('watch', ['server'], function() {
+  gulp.watch(['./src/**/*'], ['default']);
 });
