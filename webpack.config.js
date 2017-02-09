6f4ef732605da211741c6984e4d15d9b4f84ac5e
Cody Chan <int64ago@gmail.com>
2017-02-09 12:30:32 +0800

    refactor(publish): rename to NEKUI

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,38 @@
 var webpack = require('webpack');
 
 module.exports = {
-    output: {
-        filename: 'nek-ui.js',
-        library: 'RGUI',
-        libraryTarget: 'umd'
-    },
-    externals: {
-        'regularjs': {
-            root: "Regular",
-            amd: "Regular",
-            commonjs: "regularjs",
-            commonjs2: "regularjs"
-        }
-    },
-    module: {
-        loaders: [
-            {test: /\.html$/, loader: 'text-loader'},
-            {test: /\.mcss$/, loader: 'style!css!mcss'}
-        ]
-    },
-    resolve: {
-        alias: {
-            'regular-ui-base': __dirname + '/node_modules/regular-ui-base',
-            'bowser': __dirname + '/node_modules/bowser'
-        }
-    },
-    plugins: [
-        new webpack.SourceMapDevToolPlugin({
-            filename: "nek-ui.js.map",
-            columns: false
-        })
+  output: {
+    filename: 'nek-ui.js',
+    library: 'NEKUI',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    'regularjs': {
+      root: "Regular",
+      amd: "Regular",
+      commonjs: "regularjs"
+    }
+  },
+  module: {
+    loaders: [{
+        test: /\.html$/,
+        loader: 'text-loader'
+      },
+      {
+        test: /\.mcss$/,
+        loader: 'style!css!mcss'
+      }
     ]
+  },
+  resolve: {
+    alias: {
+      'bowser': __dirname + '/node_modules/bowser'
+    }
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: "nek-ui.js.map",
+      columns: false
+    })
+  ]
 }
