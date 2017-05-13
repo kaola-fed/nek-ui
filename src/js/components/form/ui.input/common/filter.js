d047a354a145b4ef911d1dfd26f2af4ac44c0dd2
hale <360783395@qq.com>
2017-05-13 12:11:01 +0800

    表单组件的required的验证也改为可配置 #62

--- src/js/components/form/ui.input/common/filter.js
+++ src/js/components/form/ui.input/common/filter.js
@@ -10,7 +10,7 @@ module.exports = {
         var match = regexp.exec(value);
         if (match) value = match[0];
 
-        if (value) value = parseInt(value);
+        if (value && !isNaN(value)) value = parseInt(value);
         return value;
     },
     /**
@@ -30,7 +30,7 @@ module.exports = {
             value = digits[0] + '.' + digits[1].substring(0, decimalDigits);
         }
   
-        if (value) value = parseFloat(value);
+        if (value && !isNaN(value)) value = parseFloat(value);
         return value;
     },
     "default": function(value) { return value; }
