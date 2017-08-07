e2e7ccc84312b26250bc933f9c555c05d64199e3
summer <wangtan@corp.netease.com>
2017-08-07 16:26:00 +0800

    misk:jira#KJDS-59474 组件示例文档的结构修改

--- doc/source/demo.wrap/index.js
+++ doc/source/demo.wrap/index.js
@@ -2,12 +2,14 @@
     demowrap组件，用来包裹组件demo的显示，实现组件代码展开和收缩
     by：wangtan@corp.netease.com
 */
+/* eslint no-unused-vars: 0 */
+/* eslint no-undef: 0 */
 const DemoWrap = Regular.extend({
-  template: '\
-    <div class="m-demowrap">\
-        <div class="m-iconLine"> <div class="m-iconOuter" r-class="{ {\'revert\':showScript}}" on-click={this.toggle()}> <kl-icon type="angle-down" /> </div> </div>\
-        <div r-html={htmlTpl} class="m-codeBlock" r-class="{ {\'wrap\': !showScript}}" ></div>\
-    </div>',
+  template: `
+    <div class="m-demowrap">
+        <div class="m-iconLine"> <div class="m-iconOuter" r-class="{ {'revert':showScript}}" on-click={this.toggle()}> <kl-icon type="angle-down" /> </div> </div>
+        <div r-html={htmlTpl} class="m-codeBlock" r-class="{ {'wrap': !showScript}}" ></div>
+    </div>`,
   name: 'demo-wrap',
   data: {
     htmlTpl: '',
@@ -17,4 +19,3 @@ const DemoWrap = Regular.extend({
     this.data.showScript = !this.data.showScript;
   },
 });
-demo
