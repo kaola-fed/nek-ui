f5c199ff10740eb9c24bfd535d216fed3c82c209
hale <hzhayue@corp.netease.com>
2017-08-23 22:25:07 +0800

    misc:save

--- doc/source/doc.js
+++ doc/source/doc.js
@@ -108,9 +108,9 @@ const doc = (isDev, callback) => {
   // 组件文档
   CATES.forEach((c) => {
     const components = getComponents(c.cate).filter((comp) => {
-      if (comp !== 'KLCrumb' && comp !== 'KLSelect' && comp !== 'KLMultiSelect' && comp !== 'KLTreeView') {
-        return false;
-      }
+      // if (comp !== 'KLCrumb' && comp !== 'KLSelect' && comp !== 'KLMultiSelect' && comp !== 'KLTreeView') {
+      //   return false;
+      // }
       const mdPath = path.join(COMPONENTS_PATH, c.cate, comp, 'index.md');
       if (fs.existsSync(mdPath)) return true;
       return false;
