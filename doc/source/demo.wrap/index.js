92131feaee3601f606b129e87e5241bdb95aa994
linxingjian <65517293@qq.com>
2017-09-19 22:03:59 +0800

    add online demo

--- doc/source/demo.wrap/index.js
+++ doc/source/demo.wrap/index.js
@@ -9,7 +9,14 @@ const DemoWrap = Regular.extend({
     <div class="m-demowrap">
         <div class="m-iconLine"> <div class="m-iconOuter" r-class="{ {'revert':showScript}}" on-click={this.toggle()}> <kl-icon type="angle-down" /> </div> </div>
         <div class="m-codeBlock" r-class="{ {'wrap': !showScript}}" >
-          <kl-button class="m-copyCode" type="tertiary" size="sm" title="复制代码" on-click={this.copy($event)}></kl-button>
+          <div class="m-operate-ctn">
+            <kl-button type="tertiary" size="sm" title="复制代码" on-click={this.copy($event)}></kl-button>
+            <kl-button type="tertiary" size="sm" title="在线运行" on-click={this.online()}></kl-button>
+            <form action="https://codepen.io/pen/define" method="POST" target="_blank" style="display:none;">
+              <input type="hidden" name="data" value={codepenJson} />
+              <input ref="onlineBtn" type="submit" />
+            </form>
+          </div>
           <div r-html={htmlTpl} class="figure-outer"></div>
         </div>
     </div>`,
@@ -17,8 +24,9 @@ const DemoWrap = Regular.extend({
   data: {
     htmlTpl: '',
     showScript: false,
-    htmlcode: '',
+    htmlCode: '',
     jsCode: '',
+    codepenJson: ''
   },
   toggle() {
     this.data.showScript = !this.data.showScript;
@@ -33,4 +41,43 @@ const DemoWrap = Regular.extend({
     body.removeChild(copyForm);
     NEKUI.KLNotify.success('复制成功');
   },
+  online: function(){
+    var btn = this.$refs.onlineBtn;
+    console.log(111,this.data)
+    var html = `
+<script>
+  var template = \`
+  {! template here !}
+  ${this.data.htmlCode.trim()}
+  \`;
+</script>
+
+<div id="app">
+</div>
+`.trim();
+    var css = `
+#app{
+  padding: 20px;
+}
+`.trim();
+    var js = `
+${this.data.jsCode.trim()}
+
+component.$inject('#app');
+`.trim();
+    var data = {
+      title: 'nekui demo',
+      description: 'this is nekui online demo',
+      html: html,
+      css: css,
+      js: js,
+      js_external: 'https://unpkg.com/nek-ui/dist/vendor/regular.min.js;https://unpkg.com/nek-ui/dist/js/nek-ui.min.js',
+      css_external: 'https://unpkg.com/nek-ui/dist/css/nek-ui.red.min.css'
+    };
+    this.data.codepenJson = JSON.stringify(data);
+    this.$update();
+    setTimeout(function(){
+      btn.click();
+    });
+  }
 });
