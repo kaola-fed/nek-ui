c885090f9ee0a9eb26b30a8166c90e6f716a3c51
hale <hzhayue@corp.netease.com>
2018-05-09 14:52:03 +0800

    misc: switch组件

--- src/js/components/form/KLSwitch/index.js
+++ src/js/components/form/KLSwitch/index.js
@@ -10,36 +10,45 @@ const template = require('./index.html');
  * @class KLSwitch
  * @extend Component
  * @param {object}      [options.data]                = 绑定属性
- * @param {boolean}      [options.data.disabled=false]      => 禁用开关
+ * @param {string}      [options.data.class]               => 补充class
+ * @param {boolean}     [options.data.disabled=false]      => 禁用开关
  * @param {boolean|string|number}      [options.data.value=false]  <=> 选择的值
  * @param {boolean|string|number} [options.data.checkedValue=true]  => 选中时的值, 用于扩展value值类型
  * @param {boolean|string|number} [options.data.unCheckedValue=false]  => 非选中时的值, 用于扩展value值类型
+ * @param {string}      [options.data.checkedText]  => 选中时的文字描述,可以是字符串模板
+ * @param {string}      [options.data.unCheckedText]  => 非选中时的文字描述,可以是字符串模板
+ * @param {string}      [options.data.size]  => 可选值sm
  */
 const KLSwitch = Component.extend({
   name: 'kl-switch',
   template,
   config() {
     this.defaults({
+      class: '',
       disabled: false,
       value: false,
       checkedValue: true,
       unCheckedValue: false,
+      checkedText: '',
+      unCheckedText: '',
+      size: '',
     });
     this.supr();
   },
   computed: {
     checked() {
-      return this.data.value === this.data.checkedValue;
+      const { data } = this;
+      return `${data.value}` === `${data.checkedValue}`;
     },
   },
   onChange() {
     const { data } = this;
-    data.value = data.value === data.checkedValue ? data.unCheckedValue : data.checkedValue;
+    data.value = `${data.value}` === `${data.checkedValue}` ? data.unCheckedValue : data.checkedValue;
 
-        /**
-         * @event KLSwitch#change 切换事件
-         * @param {boolean} value 当前value的值
-         */
+    /**
+     * @event KLSwitch#change 切换事件
+     * @param {boolean} value 当前value的值
+     */
     this.$emit('change', {
       value: data.value,
     });
--- src/js/components/form/KLSwitch/index.md
+++ src/js/components/form/KLSwitch/index.md
@@ -8,8 +8,8 @@ masonry: false
 <div class="m-example"></div>
 
 ```xml
-示例：
-<kl-switch />
+<kl-switch value={value} />
+&emsp; 当前的value值: {value}
 ```
 <!-- demo_end -->
 
@@ -18,7 +18,58 @@ masonry: false
 <div class="m-example"></div>
 
 ```xml
-示例：
 <kl-switch disabled />
+<kl-switch disabled value={value} />
+<kl-switch disabled checkedText="开" unCheckedText="关" />
+<kl-switch disabled value={value} checkedText="开" unCheckedText="关" />
+```
+
+```javascript
+var component = new NEKUI.Component({
+    template: template,
+    data: {
+        value: true
+    }
+});
+```
+<!-- demo_end -->
+
+<!-- demo_start -->
+### 扩展value值
+<div class="m-example"></div>
+
+```xml
+<kl-switch checkedValue=10 unCheckedValue=20 value={value} />
+&emsp;当前的value值: {value}
+```
+
+```javascript
+var component = new NEKUI.Component({
+    template: template,
+    data: {
+        value: 10
+    }
+});
+```
+<!-- demo_end -->
+
+<!-- demo_start -->
+### 开关描述
+<div class="m-example"></div>
+
+```xml
+文字:
+<kl-switch value={value1} checkedText="开" unCheckedText="关" />
+&emsp;图标:
+<kl-switch value={value2} checkedText="<kl-icon type='ok' />" unCheckedText="<kl-icon type='remove' />" />
+```
+<!-- demo_end -->
+
+<!-- demo_start -->
+### 小尺寸
+<div class="m-example"></div>
+
+```xml
+<kl-switch value={value1}  size="sm" />
 ```
 <!-- demo_end -->
