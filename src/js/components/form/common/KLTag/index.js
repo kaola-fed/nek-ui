b6d434d44b4e6d7e1c6ac95f9868b7722787f17b
hale <hzhayue@corp.netease.com>
2018-05-13 13:48:30 +0800

    misc: save

--- src/js/components/form/common/KLTag/index.js
+++ src/js/components/form/common/KLTag/index.js
@@ -3,14 +3,15 @@
  * @author nupthale<nupthale@163.com>
  */
 
-const Component = require('../../../ui-base/component');
+const Component = require('../../../../ui-base/component');
 const template = require('./index.html');
 
 /**
  * @class KLTag
  * @extend Component
  * @param {object}      [options.data]                = 绑定属性
- * @param {string}      [options.data.class]               => 补充class
+ * @param {string}      [options.data.class]          => 补充class
+ * @param {string}      [options.data.type=default]   => 样式, default/primary/warning
  */
 const KLTag = Component.extend({
   name: 'kl-tag',
@@ -18,6 +19,7 @@ const KLTag = Component.extend({
   config() {
     this.defaults({
       class: '',
+      type: 'default',
     });
     this.supr();
   },
---
+++ src/js/components/form/common/KLTag/index.md
@@ -0,0 +1,15 @@
+---
+title: 标签
+masonry: true
+---
+
+<!-- demo_start -->
+### 基本形式
+<div class="m-example"></div>
+
+```xml
+<kl-tag>默认标签</kl-tag>
+<kl-tag type="primary">主标签</kl-tag>
+<kl-tag type="warning">警告标签</kl-tag>
+```
+<!-- demo_end -->
